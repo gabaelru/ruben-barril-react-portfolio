8a02637033a6bfb25a72cb82b74cf057707eee10
gabaelru <ruben.barril@gmail.com>
2024-06-03 11:38:26 +0200

    Fin

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-//import { response } from 'express';
+import React, { Component } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class Login extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       email: "",
       password: "",
       errorText: ""
-    }
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,36 +20,37 @@ export default class Login extends Component {
     this.setState({
       [event.target.name]: event.target.value,
       errorText: ""
-    })
+    });
   }
 
   handleSubmit(event) {
-    axios.post("https://api.devcamp.space/sessions", 
-      {
-        client: {
-          email: this.state.email,
-          password: this.state.password
+    axios
+      .post(
+        "https://api.devcamp.space/sessions",
+        {
+          client: {
+            email: this.state.email,
+            password: this.state.password
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        if (response.data.status === "created") {
+          this.props.handleSuccessfulAuth();
+        } else {
+          this.setState({
+            errorText: "Wrong email or password"
+          });
+          this.props.handleUnsuccessfulAuth();
         }
-      },
-      { withCredentials: true}
-    )
-    .then(response => {
-      if (response.data.status === 'created') {
-        console.log("logged in return", response);
-        this.props.handleSuccessfulAuth();
-      } else {
+      })
+      .catch(error => {
         this.setState({
-          errorText: "Wrong email or password"
+          errorText: "An error occurred"
         });
         this.props.handleUnsuccessfulAuth();
-      }
-    })
-    .catch(error => {
-      this.setState({
-        errorText: "An error ocurred"
       });
-      this.props.handleUnsuccessfulAuth();
-    });
 
     event.preventDefault();
   }
@@ -61,25 +62,32 @@ export default class Login extends Component {
 
         <div>{this.state.errorText}</div>
 
-        <form onSubmit={this.handleSubmit}>
-            <input 
-              type='email'
-              name='email'
-              placeholder='Your email'
+        <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+          <div className="form-group">
+            <FontAwesomeIcon icon="envelope" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
               value={this.state.email}
               onChange={this.handleChange}
             />
-            <input 
-              type='password'
-              name='password'
-              placeholder='Your password'
+          </div>
+
+          <div className="form-group">
+            <FontAwesomeIcon icon="lock" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Your password"
               value={this.state.password}
               onChange={this.handleChange}
             />
+          </div>
 
-            <div>
-              <button type='submit'>Login</button>
-            </div>
+          <button className="btn" type="submit">
+            Login
+          </button>
         </form>
       </div>
     );
--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,14 +1,28 @@
-import { faTrash, faSignOutAlt, faEdit, 
-    faSpinner, faPlusCircle, faPhone, 
-    faEnvelope, faMapMarkedAlt } 
-from "@fortawesome/free-solid-svg-icons";
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-const Icons = () => {
-    return library.add(faTrash, faSignOutAlt, faEdit, 
-        faSpinner, faPlusCircle, faPhone, 
-        faEnvelope, faMapMarkedAlt);
-};
-
-export default Icons;
-
+import {
+    faTrash,
+    faSignOutAlt,
+    faEdit,
+    faSpinner,
+    faPlusCircle,
+    faPhone,
+    faEnvelope,
+    faMapMarkedAlt,
+    faLock
+  } from "@fortawesome/free-solid-svg-icons";
+  import { library } from "@fortawesome/fontawesome-svg-core";
+  
+  const Icons = () => {
+    return library.add(
+      faTrash,
+      faSignOutAlt,
+      faEdit,
+      faSpinner,
+      faPlusCircle,
+      faPhone,
+      faEnvelope,
+      faMapMarkedAlt,
+      faLock
+    );
+  };
+  
+  export default Icons;
